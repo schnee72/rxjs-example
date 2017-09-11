143fc4ec79bb2d9295aee5dbbaaf15104c93fd90
Dustin Schnee <schnee72@gmail.com>
2017-09-10 21:10:56 -0500

    initial

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import {Observable} from 'rxjs';
+import './index.css';
+
+let circle = document.getElementById('circle');
+let source = Observable.fromEvent(document, "mousemove")
+  .map(e => {
+    return {
+      x: e.clientX,
+      y: e.clientY
+    }
+  })
+  .filter(v => v.x < 1000)
+  .delay(300);
+
+function onNext(v) {
+  circle.style.left = v.x + "px";
+  circle.style.top = v.y + "px";
+}
+
+source.subscribe(
+  onNext,
+  e => console.log(`e: ${e}`),
+  () => console.log('complete')
+);
